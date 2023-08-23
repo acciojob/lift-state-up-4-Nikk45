113139aeeb033472e796d563ffee65234f2caecf
Nikk45 <nicksin36@gmail.com>
2023-08-23 16:43:16 +0530

    submit q4 4

--- src/components/Child.js
+++ src/components/Child.js
@@ -10,7 +10,7 @@ const Child = ({cartItems,removeItem})=>{
                 // cartItems===[] && <li>hey</li>
                 cartItems.map((item)=>{
                         return <li key={item.id}>{item.itemName} - ${item.itemPrice} 
-                    <button onClick={()=>{removeItem(item.id)}}> Remove</button></li>
+                    <button onClick={()=>{removeItem(item.id)}}>Remove</button></li>
                 })
             }
         </div>
